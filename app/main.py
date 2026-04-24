b6507bfea98df366bb1d9153706d6504a8cb59a9
Ram karthik <22a31a4437@gmail.com>
2026-04-24 18:23:00 +0530

    Fix Vercel CORS

--- app/main.py
+++ app/main.py
@@ -15,7 +15,13 @@ print("LOADED MAIN FROM:",__file__)
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","https://college-ai-chatbot-frontend.vercel.app/",],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://college-ai-chatbot-frontend.vercel.app",
+        "https://college-ai-chatbot-frontend-mmudjkmek-ramkarthik789s-projects.vercel.app",
+    ],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
