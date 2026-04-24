8b1836ca980ceaa6ab1862e643ee7da8e3f2a9dd
Ram karthik <22a31a4437@gmail.com>
2026-04-24 16:36:39 +0530

    Add vercel frontend to cors

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ print("LOADED MAIN FROM:",__file__)
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","http://127.0.0.1:5173",],
+    allow_origins=["http://localhost:5173","https://college-ai-chatbot-frontend.vercel.app/",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
